0d2f36720bc50eaed07cee71ae531a86d29d800d
woow_wu7 <woow.wu7@gmail.com>
2022-08-22 17:49:25 +0800

    feat: Link.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { Button } from "antd";
+import Link from "next/link";
 
 export default function Home() {
+  const goWebHome = () => {};
   return (
     <div className={styles.container}>
       <Head>
@@ -20,6 +23,13 @@ export default function Home() {
           Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
+
+        <div>
+          <span>导航：</span>
+          <Link href="/web-home">
+            <Button type="primary">webHome</Button>
+          </Link>
+        </div>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
